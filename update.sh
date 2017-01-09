f39986334cfb15189d6ca444a6a910716aa8fcbb
Sam Yaple <sam@yaple.net>
2017-01-09 18:33:38 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 set -u
 
-for repo in debian; do
+for repo in debian ubuntu; do
 	sed -e "s/%%DISTRO%%/${repo}/" downloader.sh > ${repo}/downloader.sh
     chmod +x ${repo}/downloader.sh
 done
