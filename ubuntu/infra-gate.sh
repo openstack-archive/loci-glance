542cf25011c9fd16f0a3889374a0057065dc1146
Sam Yaple <sam@yaple.net>
2017-04-02 19:49:32 +0000

    DONOTMERGE Remove infra mirror
    Change-Id: Idf5bc1d2c25c327b36ea4ada4fc035f63f73af3b

--- ubuntu/infra-gate.sh
+++ ubuntu/infra-gate.sh
@@ -8,10 +8,7 @@ TMPDIR=$(mktemp -d)
 mkdir -p ${TMPDIR}/etc/apt/sources.list.d
 echo 'APT::Get::AllowUnauthenticated "true";' > ${TMPDIR}/etc/apt/apt.conf
 cat << EOF > ${TMPDIR}/etc/apt/sources.list
-deb http://${NODEPOOL_MIRROR_HOST}/ubuntu xenial main restricted universe
-deb http://${NODEPOOL_MIRROR_HOST}/ubuntu xenial-updates main restricted universe
-deb http://${NODEPOOL_MIRROR_HOST}/ubuntu xenial-security main restricted universe
-deb http://${NODEPOOL_MIRROR_HOST}/ceph-deb-jewel xenial main
+deb http://download.ceph.com/debian-jewel xenial main
 EOF
 
 tar cvf override.tar -C ${TMPDIR} .
