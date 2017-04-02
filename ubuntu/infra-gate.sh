8e990b365851db3dcdb84846cb752a861b88d902
Sam Yaple <sam@yaple.net>
2017-04-02 19:47:11 +0000

    Use infra mirror for packages
    Change-Id: I245ff6d88bbf76af76a19ea78c4b31094472707a
    Depends-On: Ia664c54c2d726c03a20d8ca7f7512be3d6cfbbf9

---
+++ ubuntu/infra-gate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eux
+
+source /etc/nodepool/provider
+
+NODEPOOL_MIRROR_HOST=${NODEPOOL_MIRROR_HOST:-mirror.$NODEPOOL_REGION.$NODEPOOL_CLOUD.openstack.org}
+NODEPOOL_MIRROR_HOST=${NODEPOOL_MIRROR_HOST,,}
+
+TMPDIR=$(mktemp -d)
+mkdir -p ${TMPDIR}/etc/apt/sources.list.d
+echo 'APT::Get::AllowUnauthenticated "true";' > ${TMPDIR}/etc/apt/apt.conf
+cat << EOF > ${TMPDIR}/etc/apt/sources.list
+deb http://${NODEPOOL_MIRROR_HOST}/ubuntu xenial main restricted universe
+deb http://${NODEPOOL_MIRROR_HOST}/ubuntu xenial-updates main restricted universe
+deb http://${NODEPOOL_MIRROR_HOST}/ubuntu xenial-security main restricted universe
+deb http://${NODEPOOL_MIRROR_HOST}/ceph-deb-jewel xenial main
+EOF
+
+tar cvf override.tar -C ${TMPDIR} .
